e86ac385ad475b0f230a004001c51f8c523b34f7
YusaRyota <68580418+YusaRyota@users.noreply.github.com>
2020-07-23 06:16:55 +0900

    最初のコミットです。

---
+++ Assets/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Screen currentResolution : " + Screen.currentResolution);
+    }
+
+}
+
